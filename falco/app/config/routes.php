ec9fa4261db301ae381397cda3ca732648e7a5d2
Alexey Anufrienok <alex.anufrienok@gmail.com>
2023-04-25 06:25:18 +0300

    first commit

---
+++ falco/app/config/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    '' => [
+        'controller' => 'main',
+        'action' => 'index'
+    ],
+    'contacts' => [
+        'controller' => 'main',
+        'action' => 'contact'
+    ],
+    'sendform' => [
+        'controller' => 'main',
+        'action' => 'sendform'
+    ],
+    'account/login' => [
+        'controller' => 'account',
+        'action' => 'login'
+    ],
+    'account/register' => [
+        'controller' => 'account',
+        'action' => 'register'
+    ],
+    'news/show' => [
+        'controller' => 'news',
+        'action' => 'show'
+    ],
+];
---
+++ falco/app/controllers/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\controllers;
+use app\core\Controller;
+
+class MainController extends Controller
+{
+    public function indexAction()
+    {
+        $this->view->render('Главная страница');
+    }
+
+    public function contactAction()
+    {
+        $this->view->render('Страница контактов');
+    }
+}
